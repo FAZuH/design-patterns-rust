f5854cb756d73169886d325ae86005ffc51a76ec
FAZuH <mail@fazuh.com>
2026-06-06 16:11:50 +0700

    feat: add flyweight pattern

---
+++ structural/flyweight/main.rs
@@ -0,0 +1,34 @@
+mod without_flyweight {
+    #[derive(Default)]
+    pub struct Particle {
+        coords: (i32, i32, i32),
+        vector: (i32, i32, i32),
+        speed: i32,
+        color: (i32, i32, i32),
+        sprit: String,
+    }
+}
+
+mod with_flyweight {
+    #[derive(Default)]
+    pub struct MovingParticle {
+        coords: (i32, i32, i32),
+        vector: (i32, i32, i32),
+        speed: i32,
+    }
+
+    #[derive(Default)]
+    pub struct Particle {
+        color: (i32, i32, i32),
+        sprit: String,
+    }
+}
+
+
+fn main() {
+    // without_flyweight
+    println!("without_flyweight::Particle : {}", std::mem::size_of::<without_flyweight::Particle>() * 100_000);
+
+    // with_flyweight
+    println!("with_flyweight::Particle : {}", std::mem::size_of::<with_flyweight::Particle>() * 100_000);
+}
